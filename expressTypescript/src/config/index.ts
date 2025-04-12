1dc7faf50aa68630cf837f0722857fa70263f669
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-04-13 03:16:22 +0530

    env config understood and implemented

---
+++ expressTypescript/src/config/index.ts
@@ -0,0 +1,8 @@
+//basic config logics
+import dotenv from "dotenv"
+
+function loadenv(){
+    dotenv.config() //loads all env vraibles
+}
+
+export default loadenv
--- expressTypescript/src/server.ts
+++ expressTypescript/src/server.ts
@@ -1,14 +1,18 @@
 import express from "express"
+import loadenv from "./config"
 
 const app = express() //implicitly app is of type express.Application 
 
-const PORT:number = 3004 //explicit type definition other than this POR automatically assumes the type of PORT as the type od data you are assigning to it
+// const PORT:number = 3004 //explicit type definition other than this POR automatically assumes the type of PORT as the type od data you are assigning to it
 
 app.get('/ping', (req,res) => {
     res.send("Ping")
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
+    loadenv()
+    console.log("Env vars loaded")
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT}`)
     console.log("Press CTRL + C to stop the server")
 })
